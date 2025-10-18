fb46ed7fd0438ee3dbbe648b66e8b51484f3cf92
TheHarald <r.surinov@netris.ru>
2025-10-18 13:34:39 +0300

    Добавлен контроллер почты

--- backend/src/routes/ping.routes.ts
+++ backend/src/routes/ping.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { PingController } from "../controllers/ping.controller";
+import { sendMail } from "../controllers/email.controller";
 
 const router = Router();
 
 const pingController = new PingController();
 
 router.use("/ping", pingController.ping);
+router.post("/mail", sendMail);
 
 export { router as pingRoutes };
